f5e1778edb79a9e44b8e5a8c45e550d173d5840d
Manan Chawla <mananchawla10@gmail.com>
2023-09-06 15:28:09 +0530

    basic setup

---
+++ lua/options/init.lua
@@ -0,0 +1,12 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.hlsearch = false
+vim.opt.wrap = false
+vim.opt.tabstop = 4		
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.colorcolumn = '80'
+vim.opt.clipboard = 'unnamedplus'
+vim.opt.swapfile = false
