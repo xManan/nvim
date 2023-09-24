a88d5d97b0a2a954863498fa362151c198a1b3b4
Manan Chawla <mananchawla10@gmail.com>
2023-09-24 23:59:27 +0530

    update folder structure

---
+++ lua/keymaps.lua
@@ -0,0 +1,4 @@
+vim.g.mapleader = ' '
+vim.opt.timeoutlen = 500
+
+vim.keymap.set('n', '<leader>e', ':20Explore<cr>')
--- lua/keymaps/init.lua
+++
@@ -1,4 +0,0 @@
-vim.g.mapleader = ' '
-vim.opt.timeoutlen = 500
-
-vim.keymap.set('n', '<leader>e', ':20Lexplore<cr>')
