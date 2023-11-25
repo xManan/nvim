f0265fbc142d74e2ae6d3cd7382d4aae214e2cbf
manan chawla <mananchawla10@gmail.com>
2023-11-25 14:48:06 +0000

    updated plugins

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -40,12 +40,12 @@ return {
 			}),
 		})
 
-		cmp.setup.cmdline({ '/', '?' }, {
-			mapping = cmp.mapping.preset.cmdline(),
-			sources = {
-				{ name = 'buffer' }
-			}
-		})
+		-- cmp.setup.cmdline({ '/', '?' }, {
+		-- 	mapping = cmp.mapping.preset.cmdline(),
+		-- 	sources = {
+		-- 		{ name = 'buffer' }
+		-- 	}
+		-- })
 
 		-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
 		cmp.setup.cmdline(':', {
