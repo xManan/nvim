51a2562a9c159b2834a33bdb9f73427521a38ff5
Manan Chawla <mananchawla10@gmail.com>
2025-01-29 21:53:58 +0530

    updated ts config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,7 +5,7 @@ return {
         local configs = require("nvim-treesitter.configs")
 
         configs.setup({
-            ensure_installed = { "php", "go", "c" },
+            ensure_installed = { "php", "go", "c", "typescript", "tsx" },
             sync_install = false,
             highlight = { enable = true },
             indent = { enable = true },  
